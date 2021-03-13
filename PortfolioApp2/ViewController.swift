f1f69d2222bc551daddcb2faf4209e1c3c25733f
takayukiiiiiiiiii <suzukih551@gmail.com>
2021-03-13 21:46:04 +0900

    first commit

---
+++ PortfolioApp2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PortfolioApp2
+//
+//  Created by 鈴木隆幸 on 2021/03/13.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
